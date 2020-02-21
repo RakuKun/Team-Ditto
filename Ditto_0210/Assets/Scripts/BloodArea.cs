e9e48b8f670be2f49cae7c09d80f18ff889a0a20
maowenyu <wenyum@andrew.cmu.edu>
2020-02-21 14:18:06 -0800

    Add force zone and sliding material.

---
+++ Ditto_0210/Assets/Scripts/BloodArea.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodArea : MonoBehaviour
+{
+    public float strength;
+    public Vector3 direction;
+    private void OnTriggerStay(Collider other) 
+    {
+        
+        if(other.tag == "MotherCell" || other.tag == "Cell")
+        {
+            other.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.left*strength);
+            Debug.Log("Enter wind zone");
+        }
+    }
+}
+
